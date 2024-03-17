3862dcbe666097c4f828370163b3742a753d318a
robert <njuguiinirobert@gmail.com>
2024-03-17 21:04:19 +0300

    implement book marking game

--- bookmarks/src/main/java/dev/robert/favorites/presentation/BookmarksScreen.kt
+++ bookmarks/src/main/java/dev/robert/favorites/presentation/BookmarksScreen.kt
@@ -1,8 +1,11 @@
 package dev.robert.favorites.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun BookmarksScreen() {
+fun BookmarksScreen(
+    navController: NavController
+) {
     
 }
